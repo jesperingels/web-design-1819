814840a9357ce2d06dcd500855b9bea5c02be6b6
jesperingels <jesper.ingels@gmail.com>
2019-04-15 15:17:22 +0200

    update images

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,2 @@
-const bookMenuItem = document.getElementById('books-link');
-const bookList = document.querySelector('.collapsed');
-
-bookMenuItem.addEventListener('click', (e)=>{
-    bookList.classList.toggle('active');
-});
-
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3005;
 
 app.set('view engine', 'ejs');
 
@@ -11,21 +11,6 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.get('/nav', function(req, res) {
-    res.render('pages/nav.ejs');
-});
-
-app.get('/nav/boeken', function(req, res) {
-    res.render('pages/nav-books.ejs');
-});
-
-app.get('/kookboeken', function(req, res) {
-    res.render('pages/cook-books.ejs');
-});
-
-app.get('/succes', function(req, res) {
-    res.send('<a href="#"><h1>Test succesvol!</h1></a>');
-});
 
 app.listen(port, () => console.log('App listening on port: ' + port ));
 
