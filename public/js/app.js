48b34200e3f1922345dbcf597ee79a7e840af773
jesperingels <jesper.ingels@gmail.com>
2019-04-15 15:57:50 +0200

    add audio files

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,42 @@
+const menuItems = document.querySelectorAll('.menu-item');
+const audioBlop = document.getElementById('blop');
+const audioError = document.getElementById('error');
+
+
+menuItems.forEach((item)=>{
+    item.addEventListener('mouseenter', ()=>{
+        audioBlop.play();
+    })
+});
+
+// document.addEventListener('mouseout', (e)=>{
+//     // audioError.play();
+//     console.log('outside of window');
+//     console.log(e);
+// });
+
+function addEvent(obj, evt, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(evt, fn, false);
+    }
+    else if (obj.attachEvent) {
+        obj.attachEvent("on" + evt, fn);
+    }
+}
+
+addEvent(document, "mouseout", function(e) {
+    e = e ? e : window.event;
+    var from = e.relatedTarget || e.toElement;
+    if (!from || from.nodeName === "HTML") {
+
+        audioError.play();
+    }
+});
+
+
+
+
+
+
 
 
